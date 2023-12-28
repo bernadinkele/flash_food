ce9895223284f56414c7ab7f2f4bdc0e0504fb67
bernadinkele <kelebernadin00@gmail.com>
2023-12-28 11:04:14 +0000

    add cartview

--- lib/Presentation/Cart/cart_view.dart
+++ lib/Presentation/Cart/cart_view.dart
@@ -1,10 +1,150 @@
+import 'package:flash_food/Core/app_colors.dart';
+import 'package:flash_food/Core/response_conf.dart';
+import 'package:flash_food/Core/text_styles.dart';
+import 'package:flash_food/Presentation/Auth/screens/default_button.dart';
+import 'package:flash_food/Presentation/Base/base.dart';
+import 'package:flash_food/Presentation/Base/food_item.dart';
+import 'package:flash_food/Presentation/Cart/screens/cart_item_food.dart';
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 
 class CartView extends StatelessWidget {
   const CartView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: buildAppBar(buildContext: context, screenTitle: "My Cart"),
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: getWidth(24)),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Column(
+                children: List.generate(3, (index) => const CardItemFood()),
+              ),
+              const Gap(26),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "Recomended For You",
+                    style: TextStyles.bodyLargeSemiBold
+                        .copyWith(color: Pallete.neutral100),
+                  ),
+                  Text(
+                    "See All",
+                    style: TextStyles.bodyMediumMedium
+                        .copyWith(color: Pallete.orangePrimary),
+                  )
+                ],
+              ),
+              const Gap(16),
+              const Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  FoodItem(),
+                  FoodItem(),
+                ],
+              ),
+              const Gap(16),
+              Container(
+                height: getHeight(2),
+                width: double.infinity,
+                color: Pallete.neutral30,
+              ),
+              const Gap(16),
+              Container(
+                width: double.infinity,
+                padding: EdgeInsets.all(getSize(12)),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(getSize(16)),
+                  border: Border.all(width: 1, color: const Color(0xFFEDEDED))
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Payment Summary",
+                      style: TextStyles.bodyLargeSemiBold
+                          .copyWith(color: Pallete.neutral100),
+                    ),
+                    const Gap(8),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          "Total Items (4)",
+                          style: TextStyles.bodyMediumMedium
+                              .copyWith(color: const Color(0xFF878787)),
+                        ),
+                        Text(
+                          "\$48,900",
+                          style: TextStyles.bodyMediumBold
+                              .copyWith(color: Pallete.neutral100),
+                        ),
+                      ],
+                    ),
+                    const Gap(16),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          "Delivery Fee",
+                          style: TextStyles.bodyMediumMedium
+                              .copyWith(color: const Color(0xFF878787)),
+                        ),
+                        Text(
+                          "Free",
+                          style: TextStyles.bodyMediumBold
+                              .copyWith(color: Pallete.neutral100),
+                        ),
+                      ],
+                    ),
+                    const Gap(16),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          "Discount",
+                          style: TextStyles.bodyMediumMedium
+                              .copyWith(color: const Color(0xFF878787)),
+                        ),
+                        Text(
+                          "-\$10,900",
+                          style: TextStyles.bodyMediumBold
+                              .copyWith(color: Pallete.orangePrimary),
+                        ),
+                      ],
+                    ),
+                    const Gap(16),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          "Total",
+                          style: TextStyles.bodyMediumMedium
+                              .copyWith(color: const Color(0xFF878787)),
+                        ),
+                        Text(
+                          "\$38,000",
+                          style: TextStyles.bodyMediumBold
+                              .copyWith(color: Pallete.neutral100),
+                        ),
+                      ],
+                    ),
+
+
+                  ],
+                ),
+              ),
+              const Gap(26),
+              DefaultButton(btnContent: "Order Now"),
+              const Gap(6)
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
---
+++ lib/Presentation/Cart/screens/cart_item_food.dart
@@ -0,0 +1,128 @@
+import 'package:flash_food/Core/app_colors.dart';
+import 'package:flash_food/Core/assets_constantes.dart';
+import 'package:flash_food/Core/response_conf.dart';
+import 'package:flash_food/Core/text_styles.dart';
+import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+
+class CardItemFood extends StatelessWidget {
+  const CardItemFood({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.only(top: getHeight(16)),
+      child: Container(
+        width: double.infinity,
+        height: getHeight(108),
+        padding: EdgeInsets.all(getSize(12)),
+        decoration: ShapeDecoration(
+          color: Colors.white,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(getSize(12)),
+          ),
+          shadows: const [
+            BoxShadow(
+              color: Color(0x0A000000),
+              blurRadius: 60,
+              offset: Offset(6, 6),
+              spreadRadius: 0,
+            )
+          ],
+        ),
+        child: Row(
+          children: [
+            Checkbox(
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(getSize(4))),
+              value: true,
+              onChanged: null,
+              fillColor: const MaterialStatePropertyAll(Pallete.orangePrimary),
+            ),
+            const Gap(16),
+            Container(
+              width: getWidth(85),
+              height: getHeight(82),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(getSize(8)),
+                image: const DecorationImage(
+                  image:
+                  AssetImage(AssetsConstants.ordinaryBurger),
+                  fit: BoxFit.fill,
+                ),
+              ),
+            ),
+            const Gap(16),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                      'Burger With Meat',
+                      textAlign: TextAlign.center,
+                      style:TextStyles.bodyLargeSemiBold.copyWith(
+                          color: Pallete.neutral100
+                      )
+                  ),
+                  const Gap(4),
+                  Text(
+                      '\$ 12,230',
+                      textAlign: TextAlign.center,
+                      style: TextStyles.bodyMediumBold.copyWith(
+                          color: Pallete.orangePrimary
+                      )
+                  ),
+                  const Gap(8),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(
+                        children: [
+                          Container(
+                            height: getSize(28),
+                            width: getSize(28),
+                            decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                border:
+                                Border.all(color: const Color(0xFFEAEAEA), width: 1)),
+                            child: Icon(
+                              Icons.remove,
+                              size: getSize(24),
+                              weight: 2,
+                              color: const Color(0xFF878787),
+                            ),
+                          ),
+                          const Gap(8),
+                          Text('2',
+                              style: TextStyles.bodyLargeBold
+                                  .copyWith(color: Pallete.neutral100)),
+                          const Gap(16),
+                          Container(
+                            height: getSize(28),
+                            width: getSize(28),
+                            decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                border:
+                                Border.all(color: const Color(0xFFEAEAEA), width: 1)),
+                            child: Icon(
+                              Icons.add,
+                              size: getSize(24),
+                              weight: 2,
+                              color: Pallete.neutral100,
+                            ),
+                          ),
+                        ],
+                      ),
+                      Icon(Icons.delete, color: Pallete.pureError,size: getSize(20),)
+                    ],
+                  )
+
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
